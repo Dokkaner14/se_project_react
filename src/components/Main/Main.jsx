0df7b5354427b19d16e83e4ee592988ba9ae491b
Joe Soto <67665423+Dokkaner14@users.noreply.github.com>
2026-05-19 20:39:54 -0500

    fixed export

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,12 @@
 import WeatherCard from "../WeatherCard/WeatherCard.jsx";
 import ClothesSection from "../ClothesSection/ClothesSection.jsx";
+import "../Main/Main.css";
 
 function Main({ weatherData, clothingItems, handleCardClick }) {
   return (
     <main className="main">
       <WeatherCard weatherData={weatherData} />
-  
+
       <ClothesSection
         clothingItems={clothingItems}
         handleCardClick={handleCardClick}
